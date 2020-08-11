6b1e393c4edd8b603b2f95593e4a98b3f14b7c2f
James Rawlings <rawlingsj80@gmail.com>
2020-08-11 15:07:47 +0100

    Create variables.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,17 @@
 //--------------------------------------------------------------------
 // Modules
-# module "jx" {
-#   source  = "app.terraform.io/jenkinsxio/jx/google"
-#   gcp_project = "jenkins-x-labs-bdd"
-# }
 module "jx" {
-  source = "github.com/rawlingsj/terraform-google-jx"
+  source = "github.com/rawlingsj/terraform-google-jx2"
   gcp_project = "jenkins-x-labs-bdd"
-  jenkins_x_pipeline_bot_username = var.jenkins_x_pipeline_bot_username
-  jenkins_x_pipeline_bot_token    = var.jenkins_x_pipeline_bot_token
+  jx2 = false
 }
 
-output "jx_requirements" {
-  value = module.jx.jx_requirements
+output "connect" {
+  description = "Connect to cluster"
+  value       = "connect to your new cluster using `gcloud container clusters get-credentials ${module.jx.cluster_name} --zone ${module.jx.cluster_location} --project ${module.jx.gcp_project} && jx ns ${module.jx.jenkins_x_namespace}`"
+}
+
+output "follow" {
+  description = "Follow instructions to enable Jenkins X install via GitOps"
+  value       = "http://jenkins-x.io"
 }
